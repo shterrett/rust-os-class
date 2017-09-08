951fbfc3c4556d9ca5d2ac2c9745159443c17350
Stuart Terrett <shterrett@gmail.com>
2017-09-08 09:17:32 -0400

    redirect builtin stdout

--- ps2/src/builtin.rs
+++ ps2/src/builtin.rs
@@ -2,6 +2,8 @@ use std::collections::HashSet;
 use std::process::exit;
 use std::path::{ Path };
 use std::env::home_dir;
+use std::io::{ self, Write };
+use std::fs::File;
 
 use shell::Shell;
 use cmd_line::CmdLine;
@@ -24,14 +26,15 @@ pub fn builtin_exists(cmd_path: &str) -> bool {
 pub fn run(cmd: CmdLine, shell: &mut Shell) {
     match cmd.name {
         "exit" => exit(0),
-        "cd" => run_cd(&cmd.args, shell),
-        "pwd" => run_pwd(shell),
-        "history" => run_history(&cmd.args, shell),
+        "cd" => run_cd(&cmd, shell),
+        "pwd" => run_pwd(&cmd, shell),
+        "history" => run_history(&cmd, shell),
         _ => panic!()
     }
 }
 
-fn run_cd(args: &Vec<&str>, shell: &mut Shell) {
+fn run_cd(cmd: &CmdLine, shell: &mut Shell) {
+    let args = &cmd.args;
     let new_path =
         args.first()
             .map(|s| Path::new(s))
@@ -67,31 +70,46 @@ fn run_cd(args: &Vec<&str>, shell: &mut Shell) {
     }
 }
 
-fn run_pwd(shell: &Shell) {
+fn run_pwd(cmd: &CmdLine, shell: &Shell) {
     shell.working_dir
         .to_str()
-        .map(|dir| println!("{}", dir));
+        .map(|dir| output(format!("{}", dir), cmd.stdout));
 }
 
-fn run_history(args: &Vec<&str>, shell: &Shell) {
-    for cmd in shell.history.list(10) {
-        println!("{}", cmd);
+fn run_history(cmd: &CmdLine, shell: &Shell) {
+    for cmd_line in shell.history.list(10) {
+        output(format!("{}", cmd_line), cmd.stdout);
+    }
+}
+
+fn output(text: String, stdout: Option<&Path>) {
+    match stdout {
+        None =>  {
+            let _ = io::stdout().write(text.as_bytes());
+        },
+        Some(path) => {
+             let _ = File::create(path)
+                  .and_then(|mut f| f.write_all(text.as_bytes()));
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::run_cd;
+    use super::{ run_cd, output };
     use shell::Shell;
+    use cmd_line::CmdLine;
     use std::path::Path;
     use std::env::home_dir;
+    use std::fs::{ File, remove_file };
+    use std::io::Read;
 
     #[test]
     fn changes_working_directory_to_relative_path() {
         let mut shell = Shell::new(">", Path::new("/").to_path_buf());
-        let args = vec!["usr"];
+        let cmd = CmdLine::parse("cd usr").unwrap();
 
-        run_cd(&args, &mut shell);
+        run_cd(&cmd, &mut shell);
 
         assert_eq!(shell.working_dir, Path::new("/usr").to_path_buf());
     }
@@ -99,9 +117,9 @@ mod test {
     #[test]
     fn changes_working_directory_to_absolute_path() {
         let mut shell = Shell::new(">", Path::new("/usr").to_path_buf());
-        let args = vec!["/usr/bin"];
+        let cmd = CmdLine::parse("cd /usr/bin").unwrap();
 
-        run_cd(&args, &mut shell);
+        run_cd(&cmd, &mut shell);
 
         assert_eq!(shell.working_dir, Path::new("/usr/bin").to_path_buf());
     }
@@ -109,9 +127,9 @@ mod test {
     #[test]
     fn resolves_parent_dir_references() {
         let mut shell = Shell::new(">", Path::new("/usr/bin").to_path_buf());
-        let args = vec![".."];
+        let cmd = CmdLine::parse("cd ..").unwrap();
 
-        run_cd(&args, &mut shell);
+        run_cd(&cmd, &mut shell);
 
         assert_eq!(shell.working_dir, Path::new("/usr").to_path_buf());
     }
@@ -119,10 +137,24 @@ mod test {
     #[test]
     fn resolves_home_dir_references() {
         let mut shell = Shell::new(">", Path::new("/usr/bin").to_path_buf());
-        let args = vec!["~/"];
+        let cmd = CmdLine::parse("cd ~/").unwrap();
 
-        run_cd(&args, &mut shell);
+        run_cd(&cmd, &mut shell);
 
         assert_eq!(shell.working_dir, home_dir().unwrap());
+    }
+
+    #[test] fn redirects_std_out() {
+        let path = Path::new("test/redirect_std_out.txt");
+
+        output("Command Output".to_string(), Some(&path));
+
+        let mut output = "".to_string();
+        let _ = File::open(path)
+             .unwrap()
+             .read_to_string(&mut output);
+        assert_eq!(output, "Command Output".to_string());
+
+        let _ = remove_file(path);
     }
 }
